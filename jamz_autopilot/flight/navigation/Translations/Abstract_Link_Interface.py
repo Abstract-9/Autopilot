949f9280ccf479ee671eb69bd2c17775392211fe
ismailali1 <ismail.ali3838@gmail.com>
2021-02-09 16:49:54 -0500

    addedd abstract class linkInterface

---
+++ jamz_autopilot/flight/navigation/Translations/Abstract_Link_Interface.py
@@ -0,0 +1,21 @@
+
+
+from abc import ABC, abstractmethod
+from jamz_autopilot.flight.navigation.controller import Controller
+
+class LinkInterface (ABC) :
+
+    #Controller class variable
+    controller = Controller.initialize()
+
+    #absract method goTo directs drone go to a given location
+    def goTo(self):
+        return
+
+    #absract method ascend directs drone to ascend to a level
+    def ascend(self):
+        return
+
+    # absract method ascend directs drone to descend to a level
+    def descend(self):
+        return
