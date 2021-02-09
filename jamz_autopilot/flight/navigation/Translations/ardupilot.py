7d8c9b71222daa023e8e205bd77b2edd6010eed1
OmarRad <oradw074@uottawa.ca>
2021-02-09 14:47:30 -0500

    adding skeleton

--- jamz_autopilot/flight/navigation/Translations/ardupilot.py
+++ jamz_autopilot/flight/navigation/Translations/ardupilot.py
@@ -1,8 +1,19 @@
 
-
+from 
 
 class Ardupilot:
     def __init__(self):
         pass
 
+    def goto():
+        # returns void
+        pass
+
+    def ascend():
+        # returns void
+        pass
+
+    def descend():
+        # returns void
+        pass
     
--- jamz_autopilot/flight/navigation/controller.py
+++ jamz_autopilot/flight/navigation/controller.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+
+
+class Controller:
+    def __init__(self):
+        pass
+
+    def initialize():
+        # returns Controller
+        pass
+    
+    def setCoordinates():
+        # returns void
+        pass
