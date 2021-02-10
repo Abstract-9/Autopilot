b66ff519448a66e5d94d8ed5f9a44d0841fdaa2e
OmarRad <oradw074@uottawa.ca>
2021-02-09 21:20:39 -0500

    Omar and Ismail worked on this

--- jamz_autopilot/flight/navigation/Translations/ardupilot.py
+++ jamz_autopilot/flight/navigation/Translations/ardupilot.py
@@ -10,8 +10,6 @@ class Ardupilot(Controller):
     STATUS_DONE_COMMAND = FlightStatus(1)
     STATUS_EXECUTING_COMMAND = FlightStatus(2)
 
-    # Define drone default altitude. It'll be much higher in prod
-    DEFAULT_ALTITUDE = 5
     # Define drone ground speed in m/s.
     GROUND_SPEED = 1
     # Define the amount of time that the drone can continue its mission without talking to the controller.
@@ -42,19 +40,218 @@ class Ardupilot(Controller):
             await asyncio.sleep(1)
         print("Disarmed!")
 
+    ################# FLIGHT SECTION #################
+    # This section contains the methods for piloting the drone
 
-    def __init__(self):
-        pass
+    async def ascend(self, command):
+        self.status = self.STATUS_EXECUTING_COMMAND
 
-    def goto(self):
-        # returns void
-        pass
+        # Can't take off without arming
+        if not self.drone.armed:
+            await self.arm()
+        # Ready to go!
+        print("Taking off!")
 
-    def ascend(self):
-        # returns void
-        pass
+        self.altitude = command.alt
+        # Make sure takeoff happens
+        self.drone.simple_takeoff(command.alt)
+        while self.drone.mode.name != "GUIDED":
+            print("Taking off | Vehicle mode: {}".format(self.drone.mode.name))
+            self.drone.mode = VehicleMode("GUIDED")
+            self.drone.simple_takeoff(command.alt)
+        command.wasExecuted = True
+        await self.ensure_ascend()
+        # while True:
+        #     print(" Altitude: ", self.drone.location.global_relative_frame.alt)
+        #     # Break and return from function
+        #     print(" Altitude: ", self.drone.location.global_relative_frame.alt)
+        #     # Break and return from function just below target altitude.
+        #     if self.drone.location.global_relative_frame.alt >= command.alt * 0.95:
+        #         print("Reached target altitude: Ready for mission")
+        #         break
+        #     await asyncio.sleep(1)
 
-    def descend(self):
-        # returns void
-        pass
-    
+    async def ensure_ascend(self):
+        def current_distance():
+            return self.altitude - self.drone.location.global_relative_frame.alt
+
+        check = current_distance()
+
+        await asyncio.sleep(0.25)
+        if current_distance() < check:
+            return current_distance()
+        elif current_distance() < 0.25:  # Close enough, we're done. Goto will ensure flight altitude anyways
+            self.status = self.STATUS_DONE_COMMAND
+            return self.status
+        else:
+            self.drone.simple_takeoff(self.altitude)
+
+    async def goto(self, command):
+        self.status = self.STATUS_EXECUTING_COMMAND
+
+        self.target_location = LocationGlobalRelative(command.lat, command.lon, command.alt)
+        self.drone.simple_goto(self.target_location)
+
+        command.wasExecuted = True
+        await self.ensure_goto()
+        # while self.drone.mode.name == "GUIDED":  # Stop action if we are no longer in guided mode.
+        #     remaining_distance = get_distance_metres(self.drone.location.global_frame, target_location)
+        #     print("Distance to target: ", remaining_distance)
+        #     if remaining_distance <= target_distance * 0.01:  # Just below target, in case of undershoot.
+        #         print("Reached target")
+        #         break
+        #     time.sleep(2)
+
+    # Ensures that the drone continues to its destination.
+    async def ensure_goto(self):
+        current_location = self.drone.location.global_relative_frame
+        target_distance = get_distance_metres(current_location, self.target_location)
+
+        await asyncio.sleep(0.25)
+        remaining_distance = get_distance_metres(self.drone.location.global_frame, self.target_location)
+        print("GOTO: Remaining Distance: {}".format(remaining_distance))
+        if remaining_distance < target_distance:  # If this is true, goto is working.
+            return remaining_distance
+
+        # We will need a better method of determining arrival, maybe combine airspeed check?
+        # TODO: Yes, lets do that. implement a vector transform to get absolute velocity.
+        elif remaining_distance < 0.75:
+            self.status = self.STATUS_DONE_COMMAND
+            return self.status
+
+        # Ensure goto
+        else:
+            self.drone.simple_goto(self.target_location)
+
+    async def descend(self, command):
+        self.status = self.STATUS_EXECUTING_COMMAND
+        self.drone.mode = VehicleMode("descend")
+        command.wasExecuted = True
+        await self.ensure_descend()
+
+    async def ensure_descend(self):
+        if self.drone.mode.name == "descend":
+            if self.drone.velocity[2] < 0.05:  # It's not moving, so we've descended
+                self.status = self.STATUS_DONE_COMMAND
+                return self.status
+        else:
+            self.drone.mode = VehicleMode("descend")
+
+    # Command mapping. There's definitely a better way to do this.
+    async def execute_command(self, command):
+        # Command bindings
+        command_bindings = {
+            Command.GOTO: [self.goto, self.ensure_goto],
+            Command.ASCEND: [self.ascend, self.ensure_ascend],
+            Command.DESCEND: [self.descend, self.ensure_descend],
+            Command.RTL: self.return_home
+        }
+
+        if not command.wasExecuted:
+            await command_bindings[command.command][0](command)
+        else:
+            await command_bindings[command.command][1]()
+
+    ################# INFORMATION SECTION #################
+    # This section stores methods for accessing various information from the flight controller
+
+    def get_status(self):
+        return {
+            "GPS": self.drone.gps_0,
+            "Battery": self.drone.battery,
+            "Last Heartbeat": self.drone.last_heartbeat,
+            "Armable": self.drone.is_armable,
+            "Status": self.drone.system_status.state,
+            "Mode": self.drone.mode,
+            "Altitude": self.drone.location.global_relative_frame.alt
+        }
+
+    def get_location(self):
+        return {
+            "lat": self.drone.location.global_relative_frame.lat,
+            "lon": self.drone.location.global_relative_frame.lon,
+            "alt": self.drone.location.global_relative_frame.alt
+        }
+
+    ################# Utility section #################
+    # This section contains various utility and I/O methods
+
+    def send_heartbeat(self):
+        try:
+            data = self.get_location().update(
+                {"state": self.drone.system_status.state,
+                 "mode": self.drone.mode.name,
+                 "velocity": self.drone.velocity,
+                 "heading": self.drone.heading})
+            response = requests.post("%s/drones/%d/heartbeat" % (self.controller, self.drone_id), data=data)
+        except Exception as e:
+            print("uh oh, can't reach home. Keep flying for now...")
+            self.time_without_network += 5
+            if self.time_without_network == self.NETWORK_TIMEOUT:
+                print("network timeout reached. Returning home.")
+                self.return_home()
+            return
+        if response.status_code == 505:
+            # 505 means we need to change our ID. This should almost never happen
+            self.drone_id = response.json()['id']
+
+    def close_connection(self):
+        self.drone.close()
+
+    def __init__(self, device, drone_id, home):
+        self.drone = connect(device)
+        print("Drone connected!")
+        # Variables for talking to the controller
+        self.drone_id = drone_id
+        self.controller = home
+        self.commands = []
+        # Variables for controlling network timeout
+        self.heartbeat_counter = 0
+        self.time_without_network = 0
+        # Wait for flight controller to be ready
+        self.drone.wait_ready()
+        cmds = self.drone.commands
+        cmds.download()
+        cmds.wait_ready()
+
+        # Setup initial flight params
+        self.drone.groundspeed = self.GROUND_SPEED
+
+        # Variables for controlling flight
+        self.altitude = 0
+        self.target_location = None
+        self.status = self.STATUS_IDLE
+        self.home_location = self.drone.home_location
+        while self.home_location is None:
+            self.home_location = self.drone.home_location
+
+        print("Ready to go! Home location: %s" % self.home_location)
+
+    # The following is utility functions for various calculations
+
+    def get_distance_metres(location1, location2):
+        """
+        Returns the ground distance in metres between two LocationGlobal objects.
+
+        This method is an approximation, and will not be accurate over large distances and close to the
+        earth's poles. It comes from the ArduPilot test code:
+        https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+        """
+        dlat = location2.lat - location1.lat
+        dlong = location2.lon - location1.lon
+        return math.sqrt((dlat * dlat) + (dlong * dlong)) * 1.113195e5
+
+    def get_bearing(location1, location2):
+        """
+        Returns the bearing between the two LocationGlobal objects passed as parameters.
+
+        This method is an approximation, and may not be accurate over large distances and close to the
+        earth's poles. It comes from the ArduPilot test code:
+        https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
+        """
+        off_x = location2.lon - location1.lon
+        off_y = location2.lat - location1.lat
+        bearing = 90.00 + math.atan2(-off_y, off_x) * 57.2957795
+        if bearing < 0:
+            bearing += 360.00
+        return bearing
--- jamz_autopilot/flight/navigation/controller.py
+++ jamz_autopilot/flight/navigation/controller.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from jamz_autopilot.flight.navigation.translations. import
+from jamz_autopilot.flight.navigation.translations.Ardupilot import Ardupilot
 
 # Controller holds instance of link interface
 # Controller manages all flight variables and pass commands to 
@@ -8,20 +8,42 @@ from jamz_autopilot.flight.navigation.translations. import
 # Controller uses option lock to make one flight command go at a time
 # use lock to make sure only one command happening at time
 
-operationLock = asyncio.Lock() # this is the mutex lock
+
 
 class Controller:
     def __init__(self):
+        self.commands = []
+        self.current_command = None
+        self.operationLock = asyncio.Lock()  # this is the mutex lock
+
         pass
 
     async def initialize(self):
         # returns Controller
         # create queue, lock
 
+        # Not sure how to establish drone
+        # drone =
+
         pass
     
     def set_coordinates(self):
         # returns void
+        # functionality implemented in Command.py
         pass
 
     def main_loop(self):
+        while True:
+            if len(self.commands) == 0:
+                # Case: All commands have been executed
+                await asyncio.sleep(1)
+
+            elif self.commads and not self.operationLock.locked():
+                await self.operationLock.acquire()
+                self.current_command = self.commands.pop(0)
+                try:
+                    # drone is supposedly initialized in initialize()
+                    asyncio.create_task(drone.execute_command(self.current_command))
+                finally:
+                    self.operationLock.release()
+
