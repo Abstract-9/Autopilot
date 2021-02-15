6299d0b5d1ebc93bdbe1ca27dd07fd6046a64dd0
OmarRad <oradw074@uottawa.ca>
2021-02-15 13:29:00 -0500

    Completed FlightEvent

--- jamz_autopilot/flight/navigation/FlightEvent.py
+++ jamz_autopilot/flight/navigation/FlightEvent.py
@@ -1,4 +1,27 @@
 import asyncio
-
+from jamz_autopilot.flight.navigation.translations.Ardupilot import Ardupilot
 
 class FlightEvent(asyncio.Event):
+
+    def __init__(self):
+        super(FlightEvent, self).__init__()
+        self.battery = None
+        self.lat = None
+        self.lon = None
+        self.alt = None
+
+        self.update_vital_info()
+
+
+    def update_vital_info(self):
+
+        vitals = Ardupilot.get_vital_info()
+
+        self.battery = vitals.Battery
+        self.lat = vitals.lat
+        self.lon = vitals.lon
+        self.alt = vitals.alt
+
+        loop = asyncio.get_event_loop()
+        self.set()
+        loop.call_later(1, self.update_vital_info)
